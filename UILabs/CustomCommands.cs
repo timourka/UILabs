bbb6a8e7dae1092e30a79b2dc788d98082172b93
Timourka <timourkot@gmail.com>
2024-04-01 22:39:11 +0400

    laba3

---
+++ UILabs/CustomCommands.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace UILabs
+{
+    public static class CustomCommands
+    {
+        public static readonly RoutedUICommand command = new RoutedUICommand("command", "command", typeof(CustomCommands));
+        public static readonly RoutedUICommand zero = new RoutedUICommand("zero", "zero", typeof(CustomCommands));
+    }
+}
--- UILabs/MainWindow.xaml.cs
+++ UILabs/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +10,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace UILabs
 {
@@ -19,6 +23,23 @@ namespace UILabs
         public MainWindow()
         {
             InitializeComponent();
+            number.Text = "123456789";
+
+            CommandBinding binding = new CommandBinding(CustomCommands.command);
+            binding.Executed += NewCommand;
+            this.CommandBindings.Add(binding);
+
+            CommandBinding binding0 = new CommandBinding(CustomCommands.zero);
+            binding0.Executed += ChangeText;
+            this.CommandBindings.Add(binding0);
+        }
+        private void ChangeText(object sender, RoutedEventArgs e)
+        {
+            number.Text = "0";
+        }
+        private void NewCommand(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("New command triggered with " + e.Source.ToString());
         }
     }
 }
